1c606845c93ff1e3d273565552eeafa1bd7d27f5
Peter Shen <petershen0307@hotmail.com>
2014-12-13 23:17:40 +0800

    Writing like this is totally wrong. It declare (also define) an empty static const "MAP_STATIC123" in namespace TestMap. I got a a constant map "MAP_STATIC123" in static_map.cpp. Static mean I got a TestMap::MAP_STATIC123 copy in static_map.cpp and got a TestMap::MAP_STATIC123 copy in main.cpp.

--- staticMap.cpp
+++ staticMap.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 #include "staticMap.h"
 using namespace std;
-/*TestMap::InitialMap const TestMap::MAP_STATIC123 = TestMap::InitialMap()
+using namespace TestMap;
+InitialMap const MAP_STATIC123 = InitialMap()
         << std::pair<int, int>(1 , 1)
         << std::pair<int, int>(2 , 2)
-        << std::pair<int, int>(3 , 3);*/
+        << std::pair<int, int>(3 , 3);
 void func()
 {
-    cout << TestMap::MAP_STATIC123.at(1);
+    /*cout << TestMap::MAP_STATIC123.at(1);
     cout << TestMap::MAP_STATIC123.at(2);
-    cout << TestMap::MAP_STATIC123.at(3);
+    cout << TestMap::MAP_STATIC123.at(3);*/
     cout << endl << std::hex << &TestMap::MAP_STATIC123;
+    cout << endl << std::hex << &::MAP_STATIC123;
 }
